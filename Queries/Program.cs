5526c93ab367e835803ed0f54c3d52420eb10b14
Lawrence Wagner <lawrencewagner77@gmail.con>
2019-01-21 06:58:39 -0500

    section 4.6

--- Queries/Program.cs
+++ Queries/Program.cs
@@ -18,8 +18,9 @@ namespace Queries
                 new Movie { Title = "Star Wars V", Rating = 8.7f, Year = 1980 }
             };
 
-            var query = movies.Filter(m => m.Year >= 2000);
-            query = query.Take(1);
+            var query = movies.Filter(m => m.Year >= 2000).ToList();
+
+            Console.WriteLine(query.Count());
 
             var enumerator = query.GetEnumerator();
             while (enumerator.MoveNext())
